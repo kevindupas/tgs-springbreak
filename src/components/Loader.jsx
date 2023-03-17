cbf2a374dc1298b6d94d1dc5884636d4fdc49551
Kévin Dupas <38907117+kevindupas@users.noreply.github.com>
2023-03-17 18:24:52 +0100

    Add logo

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,7 @@ function Loader() {
     return (
         <div>
 
-            <div className="fixed w-screen h-screen bg-black flex z-50 inset-0 flex-col items-center justify-center">
+            <div className="fixed w-screen h-screen bg-[url('/dist/images/pattern-dark.jpeg')] flex z-50 inset-0 flex-col items-center justify-center">
                 <img className="w-48 animate-pulse" src="../dist/images/logo.png" alt="logo" />
             </div>
 
